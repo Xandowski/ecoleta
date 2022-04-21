098605a27c74161915261642dfacb73bb2381c66
xandowski <xandy-moraes@hotmail.com>
2022-04-21 20:43:27 -0300

    refactor: change the button component

--- web/src/components/Button/button.tsx
+++ web/src/components/Button/button.tsx
@@ -1,21 +1,24 @@
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+type KnownTags = keyof JSX.IntrinsicElements
+
 interface ButtonProps {
-    to: string
-    color: string
-    page: string
-    children: React.ReactNode
+    as?: KnownTags
+    to?: string
+    color?: string
+    size?: string
+    children?: React.ReactNode | string
   }
 
-const ButtonStyle = styled(Link)<ButtonProps>`
-    background-color: ${({theme}) => theme.colors.buttonColor};
+const ButtonStyle = css<ButtonProps>`
     background-color: ${(props) => (props.color === 'green' ? ({theme}) => theme.colors.buttonColor : ({theme}) => theme.colors.secondaryButtonColor)};
-    width: ${ (props) => (props.page == 'home' ? "360px" : "266px")};
-    height: ${ (props) => (props.page == 'home' ? "72px" : "56px")};
+    width: ${ (props) => (props.size == 'home' ? "360px" : "266px")};
+    height: ${ (props) => (props.size == 'home' ? "72px" : "56px")};
     border: none;
     font-family: ${({theme}) => theme.fonts.text};
     font-weight: ${({theme}) => theme.textWeight.title};
-    font-size: 0.75rem;
+    font-size: 0.8rem;
     color: #FFF;
     display: flex;
     align-items: center;
@@ -24,11 +27,11 @@ const ButtonStyle = styled(Link)<ButtonProps>`
     span:nth-child(1) {
         width: 72px;
         height: 100%;
-        background-color: ${(props) => (props.color === 'green' ? ({theme}) => theme.colors.buttonColorDark : ({theme}) => theme.colors.secondaryButtonColorDark)};
         border-radius: 8px 0px 0px 8px;
         display: flex;
         justify-content: center;
         align-items: center;
+        background-color: ${(props) => (props.color === 'green' ? ({theme}) => theme.colors.buttonColorDark : ({theme}) => theme.colors.secondaryButtonColorDark)};
     }
 
     span:nth-child(2) {
@@ -42,11 +45,32 @@ const ButtonStyle = styled(Link)<ButtonProps>`
     }
 `
 
-const Button = ({to, color, page, children}: ButtonProps) => {
+export const LinkButton = styled(Link)`
+    ${ButtonStyle}
+`
+
+export const InputButton = styled.input.attrs({ type: 'submit', value: 'Cadastrar ponto'})`
+    ${ButtonStyle}
+    justify-content: center;
+
+    @media(min-width: 300px) {
+        width: 100%;
+    }
+`
+
+const Button = ({to, color, size, children, as}: ButtonProps) => {
+    if (to) {
+        return (
+            <LinkButton to={to} color={color} size={size}>
+                {children}
+            </LinkButton>
+        )
+    }
+
     return (
-        <ButtonStyle to={to} color={color} page={page}>
+        <InputButton color={color} as={as}>
             {children}
-        </ButtonStyle>
+        </InputButton>
     )
 }
 

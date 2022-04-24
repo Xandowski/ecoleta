c55cf2446c5568495f56dc3df9115632317805e0
xandowski <xandy-moraes@hotmail.com>
2022-04-24 03:34:27 -0300

    feat: add Header componenet

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,23 +1,23 @@
 import Bg from '../assets/bg-home.svg'
 import logIn from '../assets/log-in.svg'
 import Button from '../components/Button/button'
-import Logo from '../components/Logo/logo'
+import Header from '../components/Header/header'
 import Main from '../styles/home'
 
 const index = () => {
     return (
         <>  
             
-            <Logo/>
+            <Header/>
             <Main>
                 <section>
                     <h1>Seu marketplace <br/> de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos <br/> de coleta de forma eficiente.</p>
-                    <Button to="/createpoint" color='green' page='Home'>
+                    <Button to="/createpoint" color='green' size='home'>
                         <span><img src={logIn} alt="log-in icon" /></span>
                         <span>Cadastre um ponto de coleta</span>
                     </Button>
-                    <Button to="/findpoint" color='purple' page='Home'>
+                    <Button to="/findpoint" color='purple' size='home'>
                         <span><img src={logIn} alt="log-in icon" /></span>
                         <span>Encontre um ponto de coleta</span>
                     </Button>

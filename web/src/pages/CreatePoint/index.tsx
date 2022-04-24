4eb18187dbd989e11b97dcb1cbe1f1c2447a6018
xandowski <xandy-moraes@hotmail.com>
2022-04-24 03:26:11 -0300

    feat: create layout structure

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,32 @@
-import { Nav } from "../../components/NavBar/navbar"
+import { useEffect } from "react";
+import Form from "../../components/Form/CreateForm";
+import Header from "../../components/Header/header";
+import Nav from "../../components/NavBar/navbar";
+import Section from "../../components/Section/section";
+import Main from "../../styles/createPoint";
 
 const CreatePoint = () => {
+  useEffect(() => {
+    document.title = "Ecoleta | Criar Ponto";  
+  }, []);
+
   return(
-    <Nav/>
+    <>
+      <Header bg="white">
+        <Nav
+          to="/findpoint"
+          title="Cadastrar ponto"
+        />
+      </Header>
+      <Main>
+        <Section>
+          <h1>Cadastre um <br/> ponto de coleta.</h1>
+
+          <Form/>
+        </Section>
+      </Main>
+      
+    </>
   )
 }
 

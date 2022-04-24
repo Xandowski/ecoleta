b2480af949af48ee8e936429abf61e3250919c1d
xandowski <xandy-moraes@hotmail.com>
2022-04-24 03:37:39 -0300

    feat: add cors and upload route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
+import cors from 'cors'
 import express from 'express'
+import path from 'path'
 import { router } from './Routes'
 
 export const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
 app.listen(3333, () => {
-  console.log('HTTP server running, https://localhost:3333')
+  console.log('HTTP server running, http://localhost:3333')
 })

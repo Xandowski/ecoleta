4b5fb209ef946e98a6f2e3e09e4be84d02ed6f6c
xandowski <xandy-moraes@hotmail.com>
2022-04-24 03:28:03 -0300

    feat: create Find Form page

--- web/src/pages/FindPoint/index.tsx
+++ web/src/pages/FindPoint/index.tsx
@@ -1,8 +1,138 @@
-import { Nav } from "../../components/NavBar/navbar"
+import { ChangeEvent, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import Form, { Select } from "../../components/Form/FindForm";
+import Header from "../../components/Header/header";
+import Nav from "../../components/NavBar/navbar";
+import Section from "../../components/Section/section";
+import Main from "../../styles/createPoint";
+
+export type UfsProps = {
+  id: number
+  nome: string
+  regiao: {id: number, sigla: string, nome: string}
+  sigla: string
+}
+
+export type CitiesProps = {
+
+}
 
 const FindPoint = () => {
+  const [ufs, setUfs] = useState<UfsProps[]>([])
+  const [selectedUf, setSelectedUf] = useState('0')
+  const [cities, setCities] = useState<string[]>([])
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm()
+
+  useEffect(() => {
+    document.title = "Ecoleta | Encontrar Ponto"  
+  }, [])
+
+  useEffect(() => {
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados', 
+    { mode: 'cors', 
+      headers: {  
+      "Content-type": "application/json; charset=UTF-8",
+      "Access-Control-Allow-Origin": "*"  
+      }
+    }).then(response => {
+        const ufInitials = response.json()
+        return ufInitials
+      })
+      .then(ufInitials => {
+        setUfs(ufInitials)
+      })
+  }, [])
+
+  useEffect(() => {
+    if(selectedUf === '0'){
+      return
+    }
+
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`, 
+    { mode: 'cors', 
+      headers: {  
+      "Content-type": "application/json; charset=UTF-8",
+      "Access-Control-Allow-Origin": "*"  
+      }
+    }).then(response => {
+        const selectedUfCities = response.json()
+        return selectedUfCities
+      })
+      .then(selectedUfCities => {
+        const cityNames = selectedUfCities.map((city) => {
+          return city.nome
+        })
+        setCities(cityNames)
+      })
+  }, [selectedUf])
+
+  
+  function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+    const uf = event.target.value
+
+    setSelectedUf(uf)
+  }
+
+  const onSubmit = (data: {uf: string, city: string}): void => {
+    //fazer a requisição para trazer os pontos
+    console.log(data)
+  }
+
   return(
-    <Nav/>
+    <>
+      <Header bg="white">
+        <Nav
+          to="/createpoint"
+          color="green"
+          title="Cadastrar ponto"
+        />
+      </Header>
+      <Main>
+        <Section>
+          <h1>Encontre um <br/> ponto de coleta.</h1>
+
+          <Form
+            onSubmitFunc={handleSubmit(onSubmit)}
+          >
+             <Select 
+                {...register('uf', { required: true })}
+                onChange={handleSelectUf}
+              >
+              {
+                ufs.map((uf, index) => {
+                  return (
+                    <option 
+                      key={index} 
+                      value={uf.sigla}
+                    >
+                      {uf.nome}
+                    </option>
+                  )
+                })
+              }
+            </Select>
+
+            <Select 
+              {...register('city', { required: true })}
+            >
+              {
+                cities.map((city, index) => {
+                  return (
+                    <option
+                      key={index} 
+                      value={city}
+                    >{city}
+                      
+                    </option>
+                  )
+                })
+              }  
+            </Select>
+          </Form>
+        </Section>
+      </Main>
+    </>
   )
 }
 

8e32665cefa5ba2260a05d03951cf9e55e2fc044
xandowski <xandy-moraes@hotmail.com>
2022-04-24 03:38:14 -0300

    feat: create routes

--- server/src/Routes.ts
+++ server/src/Routes.ts
@@ -1,17 +1,21 @@
 import { Router } from "express"
-import { celebrate, Joi } from 'celebrate'
-import { PointsController } from "./controllers/PointsController"
-
 import multer from 'multer'
 import multerConfig from './config/multer'
+import { ItemsController } from "./controllers/ItemsController"
+import { PointsController } from "./controllers/PointsController"
+
 
 const router = Router()
 const upload = multer(multerConfig)
 
 const createPoint = new PointsController()
+const getItems = new ItemsController
 
-// router.get('/point', createPoint.index)
+router.get('/point', createPoint.index)
+router.get('/items', getItems.index)
 router.post(
-  '/point', createPoint.create)
+  '/point', 
+  upload.single('image'),
+  createPoint.create)
 
 export { router }

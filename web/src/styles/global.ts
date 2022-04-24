8203a43a8c85457a8292e9016552d83af785aec1
xandowski <xandy-moraes@hotmail.com>
2022-04-24 03:33:38 -0300

    style: add global styles css

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -2,29 +2,30 @@ import { createGlobalStyle } from 'styled-components'
 
 
 export const GlobalStyles = createGlobalStyle`
-   * {
+  *,
+  *::before,
+  *::after {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
   html,
   body{
     height: 100vh;
   }
+
   a {
     text-decoration: none;
   }
 
   body {
     background-color: ${({ theme }) => theme.colors.background};
-    
-    #root{
-      height: 100vh;
-    }
   }
 
   main {
     display: flex;
+    min-height: 100vh;
   }
 
   h1, h2, h3 {
@@ -32,7 +33,7 @@ export const GlobalStyles = createGlobalStyle`
     font-weight: 700;
   }
   
-  p, label {
+  p, label, a, select {
     font-family: ${({theme}) => theme.fonts.text};
     font-weight: 400;
   }
